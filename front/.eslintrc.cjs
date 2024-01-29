c8006482ae9557f5446e65e53ff1f233fc39e63a
Han WonJun <119842443+Novrule@users.noreply.github.com>
2024-01-29 12:24:56 +0900

    [FE] 프론트엔드 프로젝트 초기 세팅 (#4)
    * chore : Vite + React + Typescript 초기 설치
    
    * feat : 로컬 호스트 주소 변경
    
    * feat : 절대 경로 설정
    
    * chore : lint 및 prettier 설정
    
    * chore : lint 및 prettier 설정
    
    * chore : styled-component 설치

---
+++ front/.eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+	root: true,
+	env: {
+		es2020: true,
+		browser: true,
+		node: true,
+	},
+	extends: [
+		"airbnb",
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		"plugin:prettier/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "prettier"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+		"prettier/prettier": ["error", { endOfLine: "auto" }],
+		"react/react-in-jsx-scope": "off",
+	},
+};
