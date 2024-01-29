735da21629ffa3666d8911daf511ad1e69e29a8f
Novrule <2000dnjswns@naver.com>
2024-01-29 12:17:03 +0900

    chore : lint 및 prettier 설정

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -1,18 +1,33 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: {
+		es2020: true,
+		browser: true,
+		node: true,
+	},
+	extends: [
+		"airbnb",
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		"plugin:prettier/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "prettier"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+		"prettier/prettier": [
+			"error",
+			{
+				endOfLine: "auto",
+			},
+		],
+		"react/react-in-jsx-scope": "off",
+	},
+};
