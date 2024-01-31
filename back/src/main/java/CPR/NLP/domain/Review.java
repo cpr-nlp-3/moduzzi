12a6b39f16fac1a1b2de6036ed8e3923182f4cf2
lee0594 <dlthdus0220@naver.com>
2024-01-31 21:25:13 +0900

    feat: DB 연결과 entity 생성

---
+++ back/src/main/java/CPR/NLP/domain/Review.java
@@ -0,0 +1,22 @@
+package CPR.NLP.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter //dto 적용 시 setter 생략하기
+@Entity
+public class Review {
+
+    @Id
+    private Long review_id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+    @Column(columnDefinition = "Text")
+    private String content;
+
+}
---
+++ back/src/test/java/CPR/NLP/NlpApplicationTests.java
@@ -0,0 +1,13 @@
+package CPR.NLP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
