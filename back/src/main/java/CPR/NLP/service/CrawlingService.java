3ec0d3925b64e1154045219f7f8bf4fdb2d6d7df
lee0594 <dlthdus0220@naver.com>
2024-02-08 21:15:47 +0900

    feat: selenium 사용한 크롤링 코드 작성, 자정마다 크롤링되도록 구현

--- back/src/main/java/CPR/NLP/service/CrawlingService.java
+++ back/src/main/java/CPR/NLP/service/CrawlingService.java
@@ -3,14 +3,22 @@ package CPR.NLP.service;
 import CPR.NLP.domain.Course;
 import CPR.NLP.domain.Review;
 import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +26,11 @@ public class CrawlingService {
 
     private final ReviewService reviewService;
     private final CourseService courseService;
+
+    @Value("${everytime.id}")
+    private String everytimeId;
+    @Value("${everytime.password}")
+    private String everytimePassword;
 
     @Scheduled(cron = "0 0 0 * * *") //반환타입이 void고, 매개변수가 없는 메소드여야 함
     public void saveReviews() {
@@ -38,31 +51,62 @@ public class CrawlingService {
 
                 reviewService.save(newReview);
             }
+            //System.out.println("reviews = " + reviews);
         }
     }
 
-    public List<String> executeCrawlingScript(String name, String professor) { //그냥 jsoup 크롤러 사용?(동적인 웹페이지에 관해서는 selenium 사용)
-        List<String> reviewList = Collections.emptyList();
+    public List<String> executeCrawlingScript(String name, String professor) { //동적인 웹페이지에 -> selenium
 
+        List<String> reviews = new ArrayList<>();
+        WebDriver driver = new ChromeDriver();
+
+        // Open the webpage
+        driver.get("https://everytime.kr/lecture");
+
+        // Fill in ID and PW and submit
+        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > form > div.input > input[type=text]:nth-child(1)")).sendKeys(everytimeId);
+        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > form > div.input > input[type=password]:nth-child(2)")).sendKeys(everytimePassword);
+        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > form > input[type=submit]")).click();
+
+        // Search for lecture
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.cssSelector("body > div > div > div.side > div > form > input[type=search]:nth-child(1)")).sendKeys(name);
+        driver.findElement(By.cssSelector("body > div > div > div.side > div > form > input.submit")).click();
+
+        // Find the professor's lecture element
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        WebElement lectureElement = null;
         try {
-            ScriptEngine engine = new ScriptEngineManager().getEngineByName("graal.js");
-
-            // JavaScript 파일 로드
-            String scriptPath = "C:/Users/dlthd/Desktop/웹_프로젝트/NLP-3/front/crawler.js";
-            engine.eval(new java.io.FileReader(scriptPath));
-
-            // JavaScript 함수 호출
-            Invocable invocable = (Invocable) engine;
-            Object reviews = invocable.invokeFunction("crawler", name, professor);
-
-            if (reviews instanceof List) {
-                reviewList = (List<String>) reviews;
-            }
-
+            lectureElement = driver.findElement(By.xpath("//div[@class='lectures']//a[@class='lecture']/div[@class='professor' and contains(text(), '" + professor + "')]"));
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Professor's lecture not found.");
+            driver.quit();
+            return reviews;
         }
 
-        return reviewList;
+        // Click on the lecture element
+        lectureElement.click();
+
+        // Find the "more" element and click it
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        WebElement moreElement = null;
+        try {
+            moreElement = driver.findElement(By.cssSelector("body > div > div > div.pane > div > section.review > div.articles > a"));
+        } catch (Exception e) {
+            System.out.println("No reviews found for the professor's lecture.");
+            driver.quit();
+            return reviews;
+        }
+        moreElement.click();
+
+        // Retrieve and print the reviews
+        for (WebElement review : driver.findElements(By.cssSelector("body > div > div > div.pane > div > div.articles > div.article > div.text"))) {
+            reviews.add(review.getText());
+        }
+
+        // Close the browser
+        driver.quit();
+
+        return reviews;
     }
 }
