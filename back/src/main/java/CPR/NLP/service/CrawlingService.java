a38475edf77e0bc5e70ea7e1ccac6940237744f8
lee0594 <dlthdus0220@naver.com>
2024-02-12 02:09:45 +0900

    refactor: 각 course를 크롤링하기 전 1회만 로그인하고 그 후 cookie 이용하도록 변경

--- back/src/main/java/CPR/NLP/service/CrawlingService.java
+++ back/src/main/java/CPR/NLP/service/CrawlingService.java
@@ -3,19 +3,13 @@ package CPR.NLP.service;
 import CPR.NLP.domain.Course;
 import CPR.NLP.domain.Review;
 import lombok.RequiredArgsConstructor;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -28,20 +22,24 @@ public class CrawlingService {
     private final IntermediateService intermediateService;
     private final CourseService courseService;
 
+    private Set<Cookie> savedCookies;
     @Value("${everytime.id}")
     private String everytimeId;
     @Value("${everytime.password}")
     private String everytimePassword;
 
-    @Scheduled(cron = "0 0 0 * * *") //반환타입이 void고, 매개변수가 없는 메소드여야 함
+    @Scheduled(cron = "0 7 2 * * *") //반환타입이 void고, 매개변수가 없는 메소드여야 함
     public void saveReviews() {
         List<Course> courses = courseService.findAll();
+        WebDriver driver = new ChromeDriver();
+
         for (Course course: courses) {
+
             String name = course.getName();
             String professor = course.getProfessor();
 
-            //여기서 crawling 함수 호출 ->  rating과 content가 담긴 reviews list 받아옴, 차례로 course_id와 함께 save
-            List<Map<String, Object>> reviews = executeCrawlingScript(name, professor);
+            List<Map<String, Object>> reviews = executeCrawlingScript(driver, name, professor); //crawling 함수 호출 ->  rating과 content가 담긴 reviews list 받아옴, 차례로 course_id와 함께 save
+
             intermediateService.deleteCourseIntermediate(course); //기존 해당 course의 intermediate 삭제
             reviewService.deleteCourseReview(course); //기존 해당 course의 review들 삭제
 
@@ -56,51 +54,53 @@ public class CrawlingService {
             }
             //System.out.println("reviews = " + reviews);
         }
+        driver.quit(); //quit 하면 cookie 정보가 모두 사라짐
     }
 
-    public List<Map<String, Object>> executeCrawlingScript(String name, String professor) { //동적인 웹페이지에 -> selenium
-
-        List<Map<String, Object>> reviews = new ArrayList<>();
-        WebDriver driver = new ChromeDriver();
+    public List<Map<String, Object>> executeCrawlingScript(WebDriver driver, String name, String professor) { //동적인 웹페이지 -> selenium 사용
 
         // Open the webpage
         driver.get("https://everytime.kr/lecture");
 
-        // Fill in ID and PW and submit
-        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > form > div.input > input[type=text]:nth-child(1)")).sendKeys(everytimeId);
-        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > form > div.input > input[type=password]:nth-child(2)")).sendKeys(everytimePassword);
-        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > form > input[type=submit]")).click();
+        if (savedCookies != null && !savedCookies.isEmpty()) {
+            for (Cookie cookie : savedCookies) {
+                driver.manage().addCookie(cookie);
+            }
+        } else {
+            login(driver);
+            savedCookies = driver.manage().getCookies();
+        }
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         // Search for lecture
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        List<Map<String, Object>> reviews = new ArrayList<>();
         driver.findElement(By.cssSelector("body > div > div > div.side > div > form > input[type=search]:nth-child(1)")).sendKeys(name);
         driver.findElement(By.cssSelector("body > div > div > div.side > div > form > input.submit")).click();
 
         // Find the professor's lecture element
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         WebElement lectureElement = null;
+
         try {
             lectureElement = driver.findElement(By.xpath("//div[@class='lectures']//a[@class='lecture']/div[@class='professor' and contains(text(), '" + professor + "')]"));
         } catch (Exception e) {
             System.out.println("Professor's lecture not found.");
-            driver.quit();
             return reviews;
         }
-
         // Click on the lecture element
         lectureElement.click();
-
-        // Find the "more" element and click it
+        
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         WebElement moreElement = null;
+        
         try {
             moreElement = driver.findElement(By.cssSelector("body > div > div > div.pane > div > section.review > div.articles > a"));
         } catch (Exception e) {
             System.out.println("No reviews found for the professor's lecture.");
-            driver.quit();
             return reviews;
         }
-        moreElement.click();
+        moreElement.click(); //더보기 메뉴
 
         // Retrieve and print the reviews
         for (WebElement review : driver.findElements(By.cssSelector("body > div > div > div.pane > div > div.articles > div.article > div.text"))) {
@@ -111,8 +111,12 @@ public class CrawlingService {
             reviews.add(evaluate);
         }
 
-        // Close the browser
-        driver.quit();
         return reviews;
+    }
+
+    private void login(WebDriver driver) {
+        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > form > div.input > input[type=text]:nth-child(1)")).sendKeys(everytimeId);
+        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > form > div.input > input[type=password]:nth-child(2)")).sendKeys(everytimePassword);
+        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > form > input[type=submit]")).click();
     }
 }
