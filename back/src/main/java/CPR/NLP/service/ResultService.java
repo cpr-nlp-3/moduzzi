4fb71a087f2ee08597594b958f1d61ad5505468f
lee0594 <dlthdus0220@naver.com>
2024-02-15 14:24:02 +0900

    feat: result DTO 생성 및 service code에 적용

--- back/src/main/java/CPR/NLP/service/ResultService.java
+++ back/src/main/java/CPR/NLP/service/ResultService.java
@@ -2,6 +2,7 @@ package CPR.NLP.service;
 
 import CPR.NLP.domain.Course;
 import CPR.NLP.domain.Result;
+import CPR.NLP.dto.ResultResponseDTO;
 import CPR.NLP.repository.ResultRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,10 +17,10 @@ public class ResultService {
 
     private final ResultRepository resultRepository;
 
-    public Result findResultByCourse(Course course) { //프론트에서 course 리스트 중 하나를 선택하고 해당 course를 parameter로 보내줌
+    public ResultResponseDTO findResultByCourse(Course course) { //프론트에서 course 리스트 중 하나를 선택하고 해당 course를 parameter로 보내줌
         Result result =  resultRepository.findByCourse(course)
                 .orElseThrow(() -> new IllegalArgumentException("해당 강의에 대한 분석 결과물이 존재하지 않습니다."));
 
-        return result;
+        return new ResultResponseDTO(result);
     }
 }
