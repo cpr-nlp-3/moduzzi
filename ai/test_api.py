b0c6c228f28bc15486f5018557edbb2e097fd902
125ryun <125ryun@gmail.com>
2024-02-20 21:26:35 +0900

    [NLP] clova summary, sentiment api test

--- ai/test_api.py
+++ ai/test_api.py
@@ -9,6 +9,7 @@ client_id = key.client_id
 client_secret = key.client_secret
 
 url = 'https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize'
+url2 = 'https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze'
 
 headers = {
             'Accept': 'application/json;UTF-8',
@@ -22,24 +23,46 @@ textfile = 'test.txt'
 textfile_path = root_path + '/ai/' + textfile
 with open(textfile_path, 'r', encoding='utf-8') as file:
     content = file.read()
+# content = re.sub('\n', ' ', content)
 
 tone = 3
-summaryCount = 3
+summaryCount = 5
+
+# data = {
+#   "document": {
+#     "content": content
+#   },
+#   "option": {
+#     "language": "ko",
+#     "model": "general",
+#     "tone": tone,
+#     "summaryCount": summaryCount,
+#   },
+# }
+
+# response = requests.post(
+#     url, 
+#     headers=headers, 
+#     data=json.dumps(data).encode('UTF-8'))
+
+# rescode = response.status_code
+# if(rescode != 200):
+#     print("Error : " + response.text)
+#     exit(1) # 에러 발생 시 프로그램 종료
+    
+# summary_raw = response.text.split(":")[1].lstrip("\"").rstrip("\"}")
+
+# summary = summary_raw.split("\\n")
+# for i, line in enumerate(summary):
+#     summary[i] = re.sub('[.,!?]', '', line)
+# print(summary)
 
 data = {
-  "document": {
     "content": content
-  },
-  "option": {
-    "language": "ko",
-    "model": "general",
-    "tone": tone,
-    "summaryCount": summaryCount,
-  },
 }
-
+json.dumps(data, indent=4, sort_keys=True)
 response = requests.post(
-    url, 
+    url2, 
     headers=headers, 
     data=json.dumps(data).encode('UTF-8'))
 
@@ -47,10 +70,6 @@ rescode = response.status_code
 if(rescode != 200):
     print("Error : " + response.text)
     exit(1) # 에러 발생 시 프로그램 종료
-    
-summary_raw = response.text.split(":")[1].lstrip("\"").rstrip("\"}")
-
-summary = summary_raw.split("\\n")
-for i, line in enumerate(summary):
-    summary[i] = re.sub('[.,!?]', '', line)
-print(summary)
+# print(response.text)
+with open("./ai/out.txt", "w") as file:
+    file.write(response.text)
