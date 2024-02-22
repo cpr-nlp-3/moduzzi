ef3c3b9730aea8eaf2058f96a58465f7c65439b0
Novrule <2000dnjswns@naver.com>
2024-02-22 15:25:40 +0900

    feat : 반응형 디자인 적용

--- front/src/styles/Globalstyles.styles.tsx
+++ front/src/styles/Globalstyles.styles.tsx
@@ -26,6 +26,46 @@ const GlobalStyles = styled.createGlobalStyle`
     font-family: "Pretendard";
     src: url(/fonts/PretendardVariable.woff2) format("woff2");
   }
+
+  html {
+    font-size: 20px;
+  }
+
+  @media (max-width: 1600px) {
+    html {
+      font-size: 18px;
+    }
+  }
+
+  @media (max-width: 1366px) {
+    html {
+      font-size: 16px;
+    }
+  }
+
+  @media (max-width: 1280px) {
+    html {
+      font-size: 14px;
+    }
+  }
+
+  @media (max-width: 1024px) {
+    html {
+      font-size: 12px;
+    }
+  }
+
+  @media (max-width: 768px) {
+    html {
+      font-size: 10px;
+    }
+  }
+
+  @media (max-width: 576px) {
+    html {
+      font-size: 8px;
+    }
+  }
 `;
 
 export default GlobalStyles;
