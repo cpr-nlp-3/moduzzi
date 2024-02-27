a29d07fda0197bf2cb96cc335cc8c0eb2f1ba225
lee0594 <64093942+lee0594@users.noreply.github.com>
2024-02-27 22:42:53 +0900

    test: 배포가 잘 됐는지 테스트하기 위한 주석 삭제 (#50)

--- back/src/main/java/CPR/NLP/repository/ReviewRepository.java
+++ back/src/main/java/CPR/NLP/repository/ReviewRepository.java
@@ -1,11 +1,8 @@
 package CPR.NLP.repository;
 
-import CPR.NLP.domain.Course;
 import CPR.NLP.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
