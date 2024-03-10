1bb4536b38797cbe6edd25b52d2fd6ce9f013abd
lee0594 <dlthdus0220@naver.com>
2024-03-11 01:23:08 +0900

    Refactor: courseService에서 local 파일 경로 jar에서도 돌아가도록 수정

--- back/src/main/java/CPR/NLP/service/CourseService.java
+++ back/src/main/java/CPR/NLP/service/CourseService.java
@@ -5,10 +5,26 @@ import CPR.NLP.dto.CourseRequestDTO;
 import CPR.NLP.dto.CourseResponseDTO;
 import CPR.NLP.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternUtils;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,25 +36,101 @@ public class CourseService {
 
     private final CourseRepository courseRepository;
 
-    public void saveOrUpdateCourse(CourseRequestDTO courseDTO) {
-        Optional<Course> existingCourse = courseRepository.findByNameAndProfessor(courseDTO.getName(), courseDTO.getProfessor());
+    /*
+     ClassPathResource resource = new ClassPathResource("개설교과목정보.xlsx");
+     String pythonScriptPath = resource.getFile().getAbsolutePath();
+     */
 
-        if (existingCourse.isEmpty()) {
-            courseRepository.save(courseDTO.toEntity());
-        } else { //어차피 과목정보에 올라오는 과목들은 거의 다 변경된 과목들이므로 따로 수정됐는지 여부는 확인하지 않아도 될 듯: 그런데 같은 교수님이 같은 과목을 두 반 이상 강의하는 경우에 location과 time 어떻게 저장할지 고려해야 할듯
-            Course existing = existingCourse.get();
-            Course updatedCourse = Course.builder()
-                    .courseId(existing.getCourseId())
-                    .code(courseDTO.getCode())
-                    .name(courseDTO.getName())
-                    .professor(courseDTO.getProfessor())
-                    .location(courseDTO.getLocation())
-                    .time(courseDTO.getTime())
-                    .createdAt(existing.getCreatedAt())
-                    .updatedAt(LocalDateTime.now())
-                    .build();
+    //private String excelFilePath = "C:\\Users\\dlthd\\Desktop\\웹_프로젝트\\NLP-3\\moduzzi\\back\\src\\main\\resources\\개설교과목정보.xlsx";
+    private Resource resource = ResourcePatternUtils.getResourcePatternResolver(new DefaultResourceLoader())
+            .getResource("classpath*:개설교과목정보.xlsx");
+    private String lastHash = null;
 
-            courseRepository.save(updatedCourse);
+    @Scheduled(cron = "0 12 0 * * MON") //매주 월요일 오후 12시에 실행 //0 12 0 * * MON
+    public void checkAndUpdateExcel() {
+        try {
+            String currentHash = calculateMD5(resource.getInputStream());
+
+            if (!currentHash.equals(lastHash)) {
+                // 파일이 업데이트되었음 -> 크롤링
+                crawlExcelFile(resource);
+                lastHash = currentHash;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String calculateMD5(InputStream inputStream) throws IOException {
+        return DigestUtils.md5Hex(inputStream);
+    }
+
+    private void crawlExcelFile(Resource resource) {
+        try {
+            //FileInputStream inputStream = new FileInputStream(new File(filePath));
+            InputStream inputStream = resource.getInputStream();
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = workbook.getSheetAt(0); // 첫 번째 시트를 가져옴
+
+            // 각 행을 순회하면서 열 값을 가져옴
+            for (int row = 1; row < sheet.getPhysicalNumberOfRows(); row++) {
+                XSSFRow currentRow = sheet.getRow(row);
+                String cell5 = currentRow.getCell(4).getStringCellValue(); // 5번째 열 (5-1=4)
+                String cell7 = currentRow.getCell(6).getStringCellValue();
+                String cell9 = currentRow.getCell(8).getStringCellValue();
+                String cell11 = currentRow.getCell(10).getStringCellValue();
+
+                Optional<Course> course = courseRepository.findByNameAndProfessor(cell5, cell11);
+                if (course.isPresent()) {
+                    Course existing = course.get();
+                    long daysDifference = ChronoUnit.DAYS.between(existing.getUpdatedAt(), LocalDateTime.now());
+                    long daysDifference2 = ChronoUnit.DAYS.between(existing.getCreatedAt(), LocalDateTime.now());
+
+                    // 업데이트 조건: updatedAt와 현재 시간의 차이, 혹은 createdAt과 현재 시간의 차이가 하루 이내인 경우: 즉 같은 과목과 교수님이 여러 분반을 가르칠 경우
+                    if (daysDifference <= 1 || daysDifference2 <= 1) {
+                        Course updatedCourse = Course.builder()
+                                .courseId(existing.getCourseId())
+                                .code(cell5)
+                                .name(cell7)
+                                .professor(cell11)
+                                .location(existing.getLocation())
+                                .time(existing.getTime() + " / " + cell9)
+                                .createdAt(existing.getCreatedAt())
+                                //.updatedAt(LocalDateTime.now())
+                                .build();
+
+                        courseRepository.save(updatedCourse);
+                    } else {
+                        Course updatedCourse = Course.builder()
+                                .courseId(existing.getCourseId())
+                                .code(cell5)
+                                .name(cell7)
+                                .professor(cell11)
+                                .location(existing.getLocation())
+                                .time(cell9)
+                                .createdAt(existing.getCreatedAt())
+                                //.updatedAt(LocalDateTime.now())
+                                .build();
+
+                        courseRepository.save(updatedCourse);
+                    }
+                } else {
+                    Course newCourse = Course.builder()
+                            .code(cell5)
+                            .name(cell7)
+                            .professor(cell11)
+                            .time(cell9)
+                            //.createdAt(LocalDateTime.now())
+                            .build();
+
+                    courseRepository.save(newCourse);
+                }
+            }
+
+            workbook.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
